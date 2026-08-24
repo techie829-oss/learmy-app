e83fc1d8b6ec50537f9b5d968d5e4d1cc59c6f58
Rohit K <rohit.kumar@ibusinessos.com>
2026-08-25 00:02:49 +0530

    build: Track compiled production assets in git

---
+++ public/build/assets/user-check-BM_HFYkd.js
@@ -0,0 +1,11 @@
+import{c as e}from"./createLucideIcon-DEOflCWW.js";/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["rect",{width:"18",height:"7",x:"3",y:"3",rx:"1",key:"f1a2em"}],["rect",{width:"9",height:"7",x:"3",y:"14",rx:"1",key:"jqznyg"}],["rect",{width:"5",height:"7",x:"16",y:"14",rx:"1",key:"q5h2i8"}]],y=e("layout-template",t);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m16 11 2 2 4-4",key:"9rsbq5"}],["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}]],h=e("user-check",c);export{y as L,h as U};
